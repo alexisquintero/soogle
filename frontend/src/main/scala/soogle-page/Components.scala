07678bc9af721fe493d919a3d7e88b8f6be326c7
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2021-07-04 14:27:53 -0300

    Simple main page

--- frontend/src/main/scala/soogle-page/Components.scala
+++ frontend/src/main/scala/soogle-page/Components.scala
@@ -5,18 +5,62 @@ import japgolly.scalajs.react.vdom.html_<^._
 
 object Search {
 
+  def search: Callback =
+    Callback.alert("Click")
+
   val searchInputs =
     ScalaComponent
       .builder[Unit]
-      .renderStatic(<.div("search"))
+      .renderStatic(
+        <.div(
+          <.input(^.tpe := "text", ^.placeholder := "Search for..."),
+          <.button(^.onClick --> search, "Search")
+        )
+      )
       .build
 }
 
 object Welcome {
+
+  val example =
+    ScalaComponent
+      .builder[Unit]
+      .renderStatic(
+        <.div(
+          ^.border := "1px solid black",
+          ^.width := 400.px,
+          ^.margin := "0 auto",
+          <.h2(
+            ^.margin := 0.px,
+            "Example searches:"
+          ),
+          <.a(^.href := "?soogle=map", ^.marginLeft := 20.px, "Map"),
+          <.p(
+            ^.marginBottom := 0.px,
+            "Enter your own search at the top of the page"
+          )
+        )
+      )
+      .build
+
   val welcome =
     ScalaComponent
       .builder[Unit]
-      .renderStatic(<.div("welcome"))
+      .renderStatic(
+        <.div(
+          <.h1("Welcome to Soogle"),
+          <.p(
+            "Soogle tries to mimic ",
+            <.a(
+              ^.href := "https://hoogle.haskell.org/",
+              ^.target := "_blank",
+              "Hoogle"
+            ),
+            "'s functionality."
+          ),
+          example()
+        )
+      )
       .build
 }
 
@@ -24,7 +68,14 @@ object Sidebar {
   val sidebar =
     ScalaComponent
       .builder[Unit]
-      .renderStatic(<.div("sidebar"))
+      .renderStatic(
+        <.a(
+          ^.width := 160.px,
+          ^.href := "https://github.com/alexisquintero/soogle",
+          ^.target := "_blank",
+          "Soogλe"
+        )
+      )
       .build
 }
 
@@ -33,7 +84,12 @@ object MainPage {
     ScalaComponent
       .builder[Unit]
       .renderStatic(
-        <.div(Sidebar.sidebar(), Search.searchInputs(), Welcome.welcome())
+        <.div(
+          ^.padding := 12.px,
+          ^.display := "flex",
+          Sidebar.sidebar(),
+          <.div(^.width := 100.pct, Search.searchInputs(), Welcome.welcome())
+        )
       )
       .build
 }
